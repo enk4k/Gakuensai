94996ea181ea1b4b6d9c5d6c51dbc62bb4e85ea4
enk4k <enkhgan06@gmail.com>
2025-10-20 16:37:28 +0900

    our feature section buren duuslaa

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require("path")
-const postCSSPlugins = [require("postcss-simple-vars")
+const postCSSPlugins = [
+    require("postcss-import")
+    , require("postcss-mixins")
+    , require("postcss-simple-vars")
     , require("postcss-nested")
     , require("autoprefixer")
 ]
@@ -10,8 +13,26 @@ module.exports = {
         filename: "bundled.js",
         path: path.resolve(__dirname, "app")
     },
+devServer: {
+    setupMiddlewares: (middlewares, devServer) => {
+        if (!devServer) {
+            throw new Error('webpack-dev-server is not defined');
+        }
+        // HTML файлуудыг хянах
+        devServer.watchFiles(["./app/**/*.html"]);
+        return middlewares;
+    },
+    static: {
+        directory: path.join(__dirname, 'app'),
+    },
+    hot: true,
+    open: true,
+    port: 9000,
+    host: "0.0.0.0"
+    
+},
     mode: "development",
-    watch: true,
+  //  watch: true,
     module: {
         rules:[
             {
